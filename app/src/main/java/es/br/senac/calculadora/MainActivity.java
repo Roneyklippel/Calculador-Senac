0c930ab8944e23b91ddffde0a925c831f52fcb24
roneyklippel <sala302b@ceines.com.br>
2019-09-10 21:26:00 -0300

    constinuação cap 4.2

--- app/src/main/java/es/br/senac/calculadora/MainActivity.java
+++ app/src/main/java/es/br/senac/calculadora/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import es.br.senac.calculadora.Calculadora
 public class MainActivity extends AppCompatActivity {
 
+    private Calculadora calc;
+    private  boolean usuarioEstaDigitandoUmNumero;
+    private boolean separadorDecimalFoiDigitado;
+    private TextView txtVisor;
     //criando a string visor
     String visor = "";
 
@@ -16,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calc =new Calculadora();
+        usuarioEstaDigitandoUmNumero= false;
+        savedInstanceState= false;
+        txtVisor=(TextView)findViewById(txtVisor);
+        txtVisor.setText("0");
 
         final TextView txtVisor = (TextView) findViewById(R.id.txtVisor);
               txtVisor.setText(visor);
